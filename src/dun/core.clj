98458e2190ea5c451414dd02a73f6389f5f2a13e
thom <chiovolonit@gmail.com>
2012-03-20 22:49:48 -0400

    fixed some smaller bugs, reordered things

--- src/dun/core.clj
+++ src/dun/core.clj
@@ -1,5 +1,7 @@
 (ns dun.core)
 
+(def char-rep {:floor ".", nil "#", :wall "#", :stairs ">", :gold "$"})
+
 (defn random
   ([n] (random 0 n))
   ([min range] (+ min (rand-int range))))
@@ -24,21 +26,18 @@
 
 (defn empty-level [w h] {:width w, :height h, :rooms #{}})
 
-(defn connect [{:keys [width height]}
-               {fx :x, fy :y, fw :w, fh :h}
-               {tx :x, ty :y, tw :w, th :h}]
+(defn connect [{:keys [width height]} {fx :x, fy :y, fw :w, fh :h} {tx :x, ty :y, tw :w, th :h}]
   (let [x0 (random fx fw), y0 (random fy fh)
         x1 (random tx tw), y1 (random ty th)
         dx (signum (- x1 x0)), dy (signum (- y1 y0))]
-    (loop [x x0, y y0, points #{}, horizontal? (boolean (= (rand-int 2) 0))]
+    (loop [x x0, y y0, points #{}, horizontal? (= (rand-int 2) 0)]
       (if (and (= x x1) (= y y1)) points
           (let [[x y] (if (or (= y y1) (and horizontal? (not= x x1))) [(+ x dx) y] [x (+ y dy)])]
             (if-not (and (> x 0) (> y 0) (< x width) (< y height)) points
-                    (recur x y
-                           (conj points [x y])
-                           (if (= 0 (rand-int 10))
-                             (boolean (not horizontal?))
-                             (boolean horizontal?)))))))))
+                    (recur x y (conj points [x y])
+                           (boolean (if (= 0 (rand-int 10))
+                                      (not horizontal?)
+                                      horizontal?)))))))))
 
 (defn connect-rooms [{:keys [width, height, rooms] :as level}]
   (loop [from (rand-elt rooms), conn #{from}, unconn (disj rooms from), paths #{}]
@@ -46,21 +45,24 @@
       (let [to (rand-elt unconn), unconn (disj unconn to), conn (conj conn to)]
         (recur (rand-elt conn), conn, unconn, (conj paths (connect level from to)))))))
 
-(defn gen-level [width height rooms]
-  (connect-rooms (nth (iterate add-room (empty-level width height)) 9)))
-
 (defn pointify [{x :x, y :y, width :w, height :h}]
   (mapcat (fn [[row y]] (map #(vector % y) row))
           (partition 2 (interleave (repeat height (range x (+ x width)))
                                    (range y (+ y height))))))
 
-(defn draw-level [{:keys [width, height, rooms, paths]}]
-  (let [lvl (merge (zipmap (mapcat pointify rooms) (repeat :floor))
-                   (zipmap (apply concat paths) (repeat :floor)))]
-    (doseq [y (range height), x (range width)]
-      (when (= 0 x) (.println System/out))
-      (.print System/out (if (lvl [x y]) "." "#")))))
+(defn finalize [{:keys [width, height, rooms, paths]}]
+  {:width width, :height height
+   :map (merge (zipmap (mapcat pointify rooms) (repeat :floor))
+               (zipmap (apply concat paths) (repeat :floor)))})
+
+(defn gen-level [width height rooms]
+  (finalize (connect-rooms (nth (iterate add-room (empty-level width height)) 9))))
+
+(defn draw-level [{width :width, height :height lvl :map}]
+  (doseq [y (range height), x (range width)]
+    (when (= 0 x) (.println System/out))
+    (.print System/out (char-rep (lvl [x y])))))
+
+(defn -main [& args] (draw-level (gen-level 80 80 30)))
 
 
-
-(defn -main [& args] (draw-level (gen-level 80 80 30))) 
