d2bcafd62fb74bc70b02d1efee806cf336acf6ff
thom <chiovolonit@gmail.com>
2012-03-24 15:34:13 -0400

    finishing touches, basically

--- src/dunjeon/core.clj
+++ src/dunjeon/core.clj
@@ -2,11 +2,18 @@
   (:import [java.awt Color Graphics2D Dimension Font]
            [javax.swing JFrame JPanel]
            [java.awt.event KeyEvent KeyAdapter])
-  (:require [clojure.set :as set]))
+  (:require [clojure.set :as set])
+  (:gen-class))
 
 (set! *warn-on-reflection* true)
 
 ;; constants and utility functions
+(def key-table
+   {[:move :east] #{KeyEvent/VK_NUMPAD6 KeyEvent/VK_D KeyEvent/VK_RIGHT KeyEvent/VK_L KeyEvent/VK_F}
+    [:move :west] #{KeyEvent/VK_NUMPAD4 KeyEvent/VK_A KeyEvent/VK_LEFT KeyEvent/VK_H KeyEvent/VK_B}
+    [:move :north] #{KeyEvent/VK_NUMPAD8 KeyEvent/VK_W KeyEvent/VK_UP KeyEvent/VK_K KeyEvent/VK_P}
+    [:move :south] #{KeyEvent/VK_NUMPAD2 KeyEvent/VK_S KeyEvent/VK_DOWN KeyEvent/VK_J KeyEvent/VK_N}
+    [:action] #{KeyEvent/VK_SPACE  KeyEvent/VK_NUMPAD5 KeyEvent/VK_PERIOD KeyEvent/VK_ENTER}})
 
 (def game-width (int 50))
 (def game-height (int 50))
@@ -17,16 +24,16 @@
 (defn abssq [[x y]] (+ (* x x) (* y y)))
 (defn dist [p0 p1] (abssq (map - p0 p1))) ; really dist^2
 
-(def panel-width (* 11 game-width))
-(def panel-height (* 11 (+ game-height 5)))
+(def panel-width (* 11 (+ 2 game-width)))
+(def panel-height (* 11 (+ game-height 10)))
 
 (def char-rep {::floor ".", nil "#", ::stairs ">", ::gold "$", ::booze "!",::player "@", ::monster "m"})
 
-(def color-rep {::floor Color/white, nil Color/gray, ::stairs Color/white, ::gold Color/yellow ::booze Color/pink
+(def color-rep {::floor Color/white, nil Color/gray, ::stairs Color/orange, ::gold Color/yellow ::booze Color/magenta
                 ::monster Color/red, ::player Color/green})
 (def auto-use #{::gold})
 (def directions {:north [0 -1], :south [0 1], :east [1 0], :west [-1 0]})
-(def distribution {::gold 5, ::booze (random 2 3), ::stairs 1})
+(def distribution {::gold 5, ::booze (random 3 4), ::stairs 1})
 
 ;; random dungeon generation
 
@@ -36,7 +43,7 @@
 
 (defn empty-map [w h] {:width w, :height h, :rooms #{}})
 (defn room [{:keys [width height]}]
-  {:x (random 2 (- width 13)) :y (random 2 (- width 13)) :w (random 3 10) :h (random 3 10)})
+  {:x (random 2 (- width 14)) :y (random 2 (- height 14)) :w (random 3 10) :h (random 3 10)})
 
 (defn add-rooms
   ([level n] (nth (iterate add-rooms level) n))
@@ -118,7 +125,7 @@
 (defn initialize-gamestate []
   (let [level (gen-level game-width game-height (random 5 4))]
     (-> {:level level
-         :player {:pos ((rand-elt (:points level)) 0),:health 50 :inv [], :sees #{} :score 0, :level 0, :dead false}
+         :player {:pos ((rand-elt (:points level)) 0),:health 50, :sees #{} :score 0, :level 0, :dead false}
          :messages ()}
         update-vision
         (add-msg Color/blue "(entering (dungeon))")
@@ -130,12 +137,14 @@
 (defmulti use-tile (fn [gs pos item] item))
 (defmethod use-tile :default [gs _ _] gs)
 (defmethod use-tile ::floor [gs _ _] gs)
-(defmethod use-tile ::booze [game-state pos _]
-  (let [healed (random 2 7)]
+(defmethod use-tile ::booze [{{h :health l :level} :player :as game-state} pos _]
+  (let [healed (random 8 8)
+        hnow (min (+ (* 5 l) 50) (+ h healed))
+        del (- hnow h)]
     (-> game-state
         (clear-tile pos)
-        (update-in [:player :health] + healed)
-        (add-msg (str "The booze heals you for " healed " points.")))))
+        (assoc-in [:player :health] hnow)
+        (add-msg (str "The booze heals you for " (if (zero? del) "no" del) " points.")))))
 
 (defmethod use-tile ::gold [game-state pos _]
   (-> game-state
@@ -162,11 +171,14 @@
   (loop [[{p :pos :as m} & ms] (seq mons)]
     (cond (= p pos) m, (not ms) nil, :else (recur ms))))
 
+(defn autoheal [{{l :level h :health} :player :as gs}]
+  (if (and (< h 50) (zero? (rand-int 5))) (update-in gs [:player :health] + (rand-int (+ l 3))) gs))
+
 (defmulti tick-player (fn [gamestate [kind & args]] kind))
 (defmethod tick-player :default [game-state _] game-state)
 (defmethod tick-player :action [{{p :pos} :player, {pts :points} :level :as gs} _] (use-tile gs p (pts p)))
 
-(defmethod tick-player :move [{{pos :pos} :player, level :level :as gs} [_ dir]]
+(defmethod tick-player :move [{{pos :pos h :health} :player, level :level :as gs} [_ dir]]
   (let [newpos (map + pos (directions dir)), tile ((:points level) newpos), mon (monster-at gs newpos)]
     (cond (not tile) gs
           mon (fight-pm gs mon)
@@ -178,9 +190,11 @@
       (add-msg Color/red (str "You have died on level " level " with " score " points."))
       (add-msg Color/red "Press enter to try again.")))
 
-(defn attack-player [gs dam]
+(defn attack-player [{{l :level h :health :as player} :player :as gs}]
   (if (zero? (rand-int 4)) (add-msg gs "You dodge the monster's attack!")
-      (let [next-gs (-> gs (update-in [:player :health] - dam) (add-msg (str "The monster attacks you for " dam " damage!")))]
+      (let [dam (random (+ l 3) (+ l 7))
+            next-gs (-> gs (update-in [:player :health] - dam)
+                        (add-msg (str "The monster attacks you for " dam " damage!")))]
         (if-not (pos? (- (:health (:player gs)) dam)) (die (assoc-in next-gs [:player :dead] true))
                 next-gs))))
 
@@ -188,28 +202,31 @@
   (let [vis (can-see? lvl mp pp),
         del (if vis (map (comp signum -) pp mp) (rand-nth (vals directions))),
         pos (map + mp del)]
-    (cond (= pos pp) (attack-player gs (random 1 6))
+    (cond (= pos pp) (attack-player gs)
           (pts pos) (-> gs (update-in [:level :monsters] disj mon) (update-in [:level :monsters] conj (assoc mon :pos pos)))
           :else gs)))
 
 (defn tick-monsters [{{ms :monsters, pts :points} :level {pp :pos} :player :as gs}] (reduce tick-monster gs ms))
 
 (defn tick [{{dead? :dead} :player :as game-state} input]
-  (cond (not dead?) (-> game-state (tick-player input) tick-monsters update-vision)
+  (cond (not dead?) (-> game-state (tick-player input) autoheal tick-monsters update-vision)
         (and dead? (= input [:action])) (initialize-gamestate)
         :else game-state))
 
-(defn comprehend [^KeyEvent input]
-  ({KeyEvent/VK_UP [:move :north], KeyEvent/VK_DOWN [:move :south], KeyEvent/VK_RIGHT [:move :east],
-    KeyEvent/VK_LEFT [:move :west], KeyEvent/VK_ENTER [:action]}
-   (.getKeyCode input)))
+(defn comprehend [code] (first (for [[key codes] key-table :when (codes code)] key)))
 
 (defn draw [^Graphics2D g {{[px py :as pos] :pos h :health s :score sees :sees, lv :level :as play} :player,
                            {:keys [points width height monsters seen]} :level, msgs :messages}]
   (doto g
     (.setColor Color/black)
     (.fillRect 0 0 panel-width panel-height)
-    (.setFont (Font. "Monospaced" Font/PLAIN 13)))
+    (.setFont (Font. "Monospaced" Font/PLAIN 13))
+    (.setColor Color/gray)
+    (.drawRect 5 5 (int (* 11 (inc game-width))) (int (* 11 (+ 2 game-height))))
+    (.drawRect 5  (int (+ 5 (* 11 (+ 2 game-height)))) (* 15 11) (* 5 11))
+    (.drawRect (+ 5 (* 15 11)) (int (+ 5 (* 11 (+ 2 game-height)))) (int (* 11 (- game-width 14))) (* 5 11))
+    (.drawRect 5 (+ 5 (int (* 11 (+ 7 game-height)))) (* 11 (inc game-width)) 22)
+    (.translate 11.0 11.0))
   (let [monpts (set/intersection sees (set (map :pos monsters)))
         post-process (fn [col sees? seen?]
                        (cond (not (or sees? seen?)) Color/black
@@ -219,17 +236,20 @@
       (let [p [xx yy],
             type (cond (= p pos) ::player, (monpts p) ::monster, :else (points p))
             p-vis (sees p), p-rem (seen p)]
-        (.setColor g (post-process (color-rep type) p-vis p-rem))
-        (.drawString g ^String (char-rep type) (int (* xx 11)) (int (* yy 11))))))
+        (when (or p-vis p-rem)
+          (.setColor g (post-process (color-rep type) p-vis p-rem))
+          (.drawString g ^String (char-rep type) (int (* xx 11)) (int (* (inc yy) 11)))))))
   (.setColor g Color/white)
   (dorun 3 (map-indexed
             (fn [i {c :col m :txt}]
-              (doto g (.setColor c) (.drawString ^String m (* 15 11) (int (* 11 (+ 4 game-height (- i))))))) msgs))
+              (doto g (.setColor c) (.drawString ^String m (* 15 11) (int (* 11 (+ (- 6 i) game-height)))))) msgs))
   (doto g
     (.setColor Color/white)
-    (.drawString (str "Health: " h) 11 (int (* 11 (inc game-height))))
-    (.drawString (str "Score: " s) 11 (int (* 11 (+ game-height 2))))
-    (.drawString (str "Level: " lv) 11 (int (* 11 (+ 3 game-height))))))
+    (.drawString (str "Health: " h) 0 (int (* 11 (+ 3 game-height))))
+    (.drawString (str "Score:  " s) 0 (int (* 11 (+ game-height 4))))
+    (.drawString (str "Level:  " lv) 0 (int (* 11 (+ 5 game-height))))
+    (.setColor Color/green)
+    (.drawString "Move with arrows/hjkl/fbnp/wasd/numpad. Use with enter/space/numpad5." 0 (int (* 11 (+ 8 game-height))))))
 
 (defn -main [& args]
   (let [game-state (atom (initialize-gamestate))
@@ -238,9 +258,9 @@
                 (.setMinimumSize dim)
                 (.setPreferredSize dim)
                 (.setMaximumSize dim))
-        ka (proxy [KeyAdapter] [] (keyPressed [e] (when-let [input (comprehend e)]
-                                                    (swap! game-state tick input)
-                                                    (.repaint panel))))]
+        ka (proxy [KeyAdapter] [] (keyPressed [^KeyEvent e] (when-let [input (comprehend (.getKeyCode e))]
+                                                              (swap! game-state tick input)
+                                                              (.repaint panel))))]
     (doto (JFrame. "(dunjeon)")
       (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
       (.add panel) .pack
